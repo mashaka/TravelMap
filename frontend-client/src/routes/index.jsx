3722a0e843fdd624b51fc643449343f2c35e4151
Pavel Dubov <dubov94@gmail.com>
2016-10-25 14:50:50 +0300

    Add map page blank

--- frontend-client/src/routes/index.jsx
+++ frontend-client/src/routes/index.jsx
@@ -4,11 +4,13 @@ import {Route, IndexRedirect } from "react-router";
 import App from "../containers/App";
 import LoginView from "../views/LoginView"
 import SignUpView from "../views/SignUpView"
+import MapView from "../views/MapView"
 
 export default(
     <Route path="/" component={App}>
         <IndexRedirect to="login" />
         <Route path="login" component={LoginView} />
         <Route path="signup" component={SignUpView} />
+        <Route path="map" component={MapView} />
     </Route>
 );
---
+++ frontend-client/src/views/MapView.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "../styles/MapView.scss"
+
+export default class MapView extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello, world!
+            </div>
+        )
+    }
+}
